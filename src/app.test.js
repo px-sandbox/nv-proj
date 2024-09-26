ee286dd4b5e5ea68f40c518e815764c070d77351
NarendraSG <narendra@studiographene.com>
2024-09-26 11:43:28 +0000

    fixes

---
+++ src/app.test.js
@@ -0,0 +1,33 @@
+describe('ClientController (e2e)', () => {
+
+
+	it('/client (POST) create client', async () => {
+		expect(2).toBe(2);
+    });
+
+    it('/client (POST) create client', async () => {
+		expect(2).toBe(2);
+    });
+
+    it('/client (POST) create client', async () => {
+		expect(2).toBe(2);
+    });
+
+    it('/client (POST) create client', async () => {
+		expect(2).toBe(2);
+    });
+
+    it('/client (POST) create client', async () => {
+		expect(2).toBe(2);
+    });
+
+    it('/client (POST) create client', async () => {
+		expect(2).toBe(2);
+    });
+
+    it('/client (POST) create client', async () => {
+		expect(2).toBe(2);
+    });
+
+
+});
